b0c684b231014ccb078f9b7f70c4ccd60d6a46b8
elishaadamu <elishadamu97@gmail.com>
2025-06-26 08:32:23 +0100

    first commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,11 +4,14 @@ import { gsap } from "gsap";
 import "./Styles/hero.scss";
 import Me from "../assets/elisha.jpg";
 import { Link } from "react-scroll";
+import Resume from "../assets/resume.pdf"; // Adjust the path as necessary
 import Text from "./Text";
 
 function Hero() {
   const div1 = useRef(null);
   const div2 = useRef(null);
+
+  const resumeLink = Resume;
 
   useEffect(() => {
     const timeline = gsap.timeline();
@@ -51,16 +54,27 @@ function Hero() {
           <h1>Hi, I'm Zak Mumuni</h1>
           <Text />
           <p>
-            Specializing in creating sleek, responsive, and user-friendly web
-            applications. Dedicated to delivering seamless user experiences with
-            a focus on innovative design, attention to detail, and ensuring your
-            website stands out and engages your audience.
+            As a <strong>Senior Data Analyst</strong>, I translate complex
+            datasets into actionable insights that drive strategic business
+            decisions. With a strong foundation in statistical modeling, data
+            visualization, and advanced SQL, I excel at uncovering hidden trends
+            and crafting compelling data narratives. My passion lies in
+            empowering organizations to leverage their data effectively, leading
+            to measurable improvements in efficiency, profitability, and
+            customer satisfaction.
           </p>
 
-          <div className="btns">
-            <a href="mailto:zmumuni.da@gmail.com" target="_blank">
-              Contact
-            </a>
+          <div className="flex gap-5 justify-start ">
+            <div className="btns">
+              <a href={resumeLink} target="_blank" download>
+                Resume
+              </a>
+            </div>
+            <div className="btns">
+              <a href="mailto:zmumuni.da@gmail.com" target="_blank">
+                Contact
+              </a>
+            </div>
           </div>
         </div>
 
