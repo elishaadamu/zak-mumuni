2068d71626ad6ae238e2aae6bc1bba4f48c111c7
elishaadamu <elishadamu97@gmail.com>
2025-07-06 21:07:07 +0100

    edit home.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -21,59 +21,9 @@ function Portfolio() {
 
   const works = [
     {
-      workImg: TS,
-      name: "Time Series Forecasting",
-      index: "01",
-
-      link: "https://github.com/zakariDS/portfolio/blob/main/DS_forecasting.ipynb",
-      description:
-        "Predicted future freeway traffic congestion using LSTM and SARIMA models for St. Louis, Missouri Metropolitan Area. ",
-    },
-    {
-      workImg: clustering,
-      name: "Clustering",
-      index: "02",
-
-      link: "https://github.com/zakariDS/portfolio/blob/main/DS_clustering_Analysis.ipynb",
-      description:
-        "I used k-means clustering to analyze the concentration  of socio-economically disadvantaged groups in Honolulu, Hawaii Metropolitan Area.",
-    },
-    {
-      workImg: PCA,
-      name: "Principal Components Analysis",
-      index: "03",
-      link: "https://github.com/zakariDS/portfolio/blob/main/DS_pca.ipynb",
-      description:
-        "I used PCA to combine various factors that were used to measure transportation disadvantage into a one-dimensional factor that explained most of the variance in the data for, Honolulu, Hawaii Metropolitan Area.",
-    },
-    {
-      workImg: Classification,
-      name: "Classification",
-      index: "04",
-      link: "https://github.com/zakariDS/portfolio/blob/main/DS_classification.ipynb",
-      description:
-        "I used binary logistic regression, KNN, SVM, and Decision Trees to predict the likelihood of roadways being congested in St. Louis, Missouri Metropolitan Area.",
-    },
-    {
-      workImg: Impact,
-      name: "Impact Analysis",
-      index: "05",
-      link: "https://github.com/zakariDS/portfolio/blob/main/IA_impact.ipynb",
-      description:
-        "I employed Bayesian Structural Time-Series (BSTS) model to assess the impact of work zones on urban freeway traffic congestion.",
-    },
-    {
-      workImg: Markov,
-      name: "Conjestion Transition ",
-      index: "06",
-      link: "https://github.com/zakariDS/portfolio/blob/main/TA_congestion_transition.ipynb",
-      description:
-        "I used Markov chain model to analyze the transition from one state of urban traffic congestion to another. ",
-    },
-    {
       workImg: DataPipeline,
       name: "Data Pipeline Orchestration",
-      index: "07",
+      index: "01",
       link: "https://github.com/zmtcampo/traffic_pipeline_dbt_airflow",
       description:
         "Built data pipelines using Python, DBT, and Airflow for real-time traffic analysis",
@@ -81,7 +31,7 @@ function Portfolio() {
     {
       workImg: TrafficAnalysis,
       name: "Traffic Analytics Project",
-      index: "08",
+      index: "02",
       link: "https://github.com/zmtcampo/Traffic-Analysis-Project",
       description:
         "This data analysis project aims to provide insights into the traffic performance of EWG metropolitan region, located between Missouri and Illinois.",
@@ -89,7 +39,7 @@ function Portfolio() {
     {
       workImg: KPIMonitoring,
       name: "KPI Monitoring",
-      index: "09",
+      index: "03",
       link: "https://github.com/zmtcampo/react_dashboard",
       description:
         "Built a custom analytics dashboard using Python and React.js to monitor KPIs, adopted company-wide by 5 teams for data-driven decision",
@@ -97,7 +47,7 @@ function Portfolio() {
     {
       workImg: TimeSeries,
       name: "Time Series Prediction",
-      index: "10",
+      index: "04",
       link: "https://github.com/zmtcampo/time-series-forecasting/blob/main/DS_forecasting.ipynb",
       description:
         "Developed LSTM and SARIMA models to predict future traffic congestion in the St. Louis, MI metropolitan area",
@@ -105,7 +55,7 @@ function Portfolio() {
     {
       workImg: ClusterAnalysis,
       name: "Cluster Analysis",
-      index: "11",
+      index: "05",
       link: "https://github.com/zmtcampo/cluster_analysis/blob/main/DS_clustering_Analysis.ipynb",
       description:
         "Implemented binary logistic regression, KNN, SVM, and Decision Trees in Python to predict roadway congestion in the St. Louis, Missouri metropolitan area",
@@ -113,7 +63,7 @@ function Portfolio() {
     {
       workImg: CountyData,
       name: "County Data Analytics.",
-      index: "12",
+      index: "06",
       link: "https://github.com/zmtcampo/fc-data-project",
       description:
         "Reduced ad-hoc data requests by building self-service analytics apps with Shiny",
